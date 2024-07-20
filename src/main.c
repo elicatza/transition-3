2292b06e1eedc04ca87637250cc84cddda44bdc3
elicatza <eliza.cl@protonmail.com>
2024-07-20 12:01:15 +0200

    feat: you can select sides now
    It look terrible now.

--- src/main.c
+++ src/main.c
@@ -29,7 +29,15 @@ typedef struct {
     ButtonKind kind;
 } Button;
 
-typedef Vector2 Player;
+typedef enum {
+    PREVIEW,
+    PHYSICAL,
+} PlayerState;
+
+typedef struct {
+    Vector2 pos;
+    PlayerState state;
+} Player;
 
 /**
  * All values are to be defined in world space
@@ -43,6 +51,7 @@ typedef struct {
     Button *button_case;
     Rectangle rec;
     float padding;
+    int clicked_button;  /* id if button is clicked. Else -1 */
 } Puzzle;
 
 typedef struct {
@@ -81,14 +90,15 @@ int main(void)
         .rows = 10,
         .cells = NULL,
         .button_case = case_init(64, sizeof (Button)),
-        .padding = 20.f,
+        .padding = 30.f,
+        .clicked_button = -1,
     };
-    case_push(puzzle.player_case, ((Vector2) { 0.f, 0.f } ));
+    case_push(puzzle.player_case, ((Player) { .pos = (Vector2) { 0.f, 0.f }, .state = PHYSICAL } ));
     go.heart = LoadTextureFromImage(heart_img);
     go.puzzle = puzzle;
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(render, 0, 1);
+    emscripten_set_main_loop(loop, 0, 1);
 #else
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -113,69 +123,67 @@ typedef enum {
     RIGHT = KEY_D,
 } Direction;
 
+/**
+ * O(n)
+ */
+bool is_valid_pos(Puzzle *p, Vector2 pos)
+{
+    if (!(pos.x >= 0 && pos.x < p->cols)) { return false; }
+    if (!(pos.y >= 0 && pos.y < p->rows)) { return false; }
+
+    size_t i;
+    for (i = 0; i < case_len(p->player_case); ++i) {
+        if ((int) pos.x + 0.5f == (int) p->player_case[i].pos.x + 0.5f &&
+            (int) pos.y + 0.5f == (int) p->player_case[i].pos.y + 0.5f) {
+            // Colliding with other object
+            return false;
+        }
+    }
+    return true;
+}
+
 void move_square(Puzzle *p, Player *player, Direction dir)
 {
     // TODO play animation when can't move + when move
-    Vector2 new_pos = *player;
+    Vector2 new_pos = player->pos;
     switch (dir) {
-        case UP: {
-            if (player->y > 0) {
-                new_pos.y += -1;
-            } else { return; }
-        } break;
-        case DOWN: {
-            if (player->y + 1 < p->rows) {
-                new_pos.y += 1;
-            } else { return; }
-        } break;
-        case LEFT: {
-            if (player->x > 0) {
-                new_pos.x += -1;
-            } else { return; }
-        } break;
-        case RIGHT: {
-            if (player->x + 1 < p->cols) {
-                new_pos.x += 1;
-            } else { return; }
-        } break;
+        case UP: { new_pos.y += -1; } break;
+        case DOWN: { new_pos.y += 1; } break;
+        case LEFT: { new_pos.x += -1; } break;
+        case RIGHT: { new_pos.x += 1; } break;
             default: {
             INFO("This function should not be run");
         } break;
     };
 
     // Check collisions
-    size_t i;
-    for (i = 0; i < case_len(p->player_case); ++i) {
-        if ((int) new_pos.x + 0.5f == (int) p->player_case[i].x + 0.5f &&
-            (int) new_pos.y + 0.5f == (int) p->player_case[i].y + 0.5f) {
-            // Colliding with other object
-            return;
-        }
+    if (is_valid_pos(p, new_pos)) {
+        player->pos.x = new_pos.x;
+        player->pos.y = new_pos.y;
     }
-    player->x = new_pos.x;
-    player->y = new_pos.y;
 }
 
 int players_cmp_ud(const void *a, const void *b)
 {
-    return ((Player *) a)->y - ((Player *) b)->y;
+    return ((Player *) a)->pos.y - ((Player *) b)->pos.y;
 }
 
 int players_cmp_du(const void *a, const void *b)
 {
-    return ((Player *) b)->y - ((Player *) a)->y;
+    return ((Player *) b)->pos.y - ((Player *) a)->pos.y;
 }
 
 int players_cmp_lr(const void *a, const void *b)
 {
-    return ((Player *) a)->x - ((Player *) b)->x;
+    return ((Player *) a)->pos.x - ((Player *) b)->pos.x;
 }
 
 int players_cmp_rl(const void *a, const void *b)
 {
-    return ((Player *) b)->x - ((Player *) a)->x;
+    return ((Player *) b)->pos.x - ((Player *) a)->pos.x;
 }
 
+typedef int (*__compar_fn_t)(const void *, const void *);
 void loop(void)
 {
     Puzzle *p = &go.puzzle;
@@ -218,8 +226,6 @@ void loop(void)
 void fill_buttons(Puzzle *p)
 {
     case_len(p->button_case) = 0;
-    float cell_width = p->rec.height / p->cols;
-    size_t i;
 
     size_t row;
     for (row = 1; row < p->rows; ++row) {
@@ -254,14 +260,10 @@ void fill_buttons(Puzzle *p)
     }
 }
 
-void render_buttons(Puzzle *p)
+void render_button(Puzzle *p, Button btn)
 {
-    size_t i;
-    float cell_width = p->rec.width / p->cols;
-    for (i = 0; i < case_len(p->button_case); ++i) {
-        Button vs_btn = vs_button_of_ws(p, p->button_case[i]);
-        DrawCircleV(vs_btn.center, vs_btn.radius, GREEN);
-    }
+    Button vs_btn = vs_button_of_ws(p, btn);
+    DrawCircleV(vs_btn.center, vs_btn.radius, GREEN);
 }
 
 Vector2 vec2d_add(Vector2 a, Vector2 b)
@@ -310,8 +312,11 @@ Player vs_player_of_ws(Puzzle *p, Player player)
 {
     float cell_width = p->rec.width / p->cols;
     return (Player) {
-        .x = player.x * cell_width + p->rec.x,
-        .y = player.y * cell_width + p->rec.y,
+        .pos = {
+            .x = player.pos.x * cell_width + p->rec.x,
+            .y = player.pos.y * cell_width + p->rec.y,
+        },
+        .state = player.state,
     };
 }
 
@@ -322,7 +327,6 @@ int button_hover_id(Puzzle *p, Button *btn_case)
 {
     size_t i;
     Vector2 pos = GetMousePosition();
-    float cell_width = p->rec.width / p->cols;
     for (i = 0; i < case_len(btn_case); ++i) {
         Button vs_button = vs_button_of_ws(p, btn_case[i]);
         if (in_circle(vs_button.center, vs_button.radius, pos)) {
@@ -332,35 +336,111 @@ int button_hover_id(Puzzle *p, Button *btn_case)
     return -1;
 }
 
-void mirror_over_line(Puzzle *p, int button_id)
+#define MIRROR_UP (1 << 0)
+#define MIRROR_DOWN (1 << 1)
+#define MIRROR_LEFT (1 << 2)
+#define MIRROR_RIGHT (1 << 3)
+#define MIRROR_PREVIEW (1 << 4)
+
+void render_player(Puzzle *p, Player player);
+
+/**
+ * @param options. Check MIRROR_ namespace for options, 
+ */
+void mirror_over_line(Puzzle *p, int button_id, int options)
 {
-    Button btn = p->button_case[button_id];
-    float cell_width = p->rec.width / p->cols;
-    if (btn.center.x == 0.f) {
-        // Horizontal mirror
-        Vector2 mirrored = {
-            .x = p->player_case[0].x,
-            .y = (2 * btn.center.y) - p->player_case[0].y - 1,
-        };
-        if (mirrored.y < p->rec.height / cell_width) {
-            // In bound of puzzle
-            case_push(p->player_case, mirrored);
-            INFO("added new at %.0f, %.0f", mirrored.x, mirrored.y);
+    ASSERT(options != 0, "Invalid options");
+    Button ws_btn = p->button_case[button_id];
+
+
+    size_t len = case_len(p->player_case);
+    size_t i;
+    for (i = 0; i < len; ++i) {
+
+        Vector2 mirrored = { 0 };
+        if (options & MIRROR_UP || options & MIRROR_DOWN) {
+            // Horizontal mirror
+            mirrored = (Vector2) {
+                .x = p->player_case[i].pos.x,
+                .y = (2 * ws_btn.center.y) - p->player_case[i].pos.y - 1,
+            };
+            if ((mirrored.y >= ws_btn.center.y && options & MIRROR_UP) ||
+                (mirrored.y < ws_btn.center.y && options & MIRROR_DOWN)) {
+                continue;
+            }
+        } else if (options & MIRROR_RIGHT || options & MIRROR_LEFT) {
+            // Vertical mirror
+            mirrored = (Vector2) {
+                .x = (2 * ws_btn.center.x) - p->player_case[i].pos.x - 1,
+                .y = p->player_case[i].pos.y,
+            };
+            if ((mirrored.x >= ws_btn.center.x && options & MIRROR_LEFT) ||
+                (mirrored.x < ws_btn.center.x && options & MIRROR_RIGHT)) {
+                continue;
+            }
         }
-    } else if (btn.center.y == 0.f) {
-        // Vertical mirror
-        Vector2 mirrored = {
-            .x = (2 * btn.center.x) - p->player_case[0].x - 1,
-            .y = p->player_case[0].y,
-        };
-        if (mirrored.x < p->rec.width / cell_width) {
-            // In bound of puzzle
-            case_push(p->player_case, mirrored);
-            INFO("added new at %.0f, %.0f", mirrored.x, mirrored.y);
+
+        if (is_valid_pos(p, mirrored)) {
+            if (options & MIRROR_PREVIEW) {
+                // Vector2 size = vec2d_sub(*(Vector2 *) &p->rec.width, vs_btn.center);
+                // Draw rectangle over section
+                Player mirr_player = { .pos = mirrored, .state = PREVIEW };
+                case_push(p->player_case, mirr_player);
+            } else {
+                // In bound of puzzle
+                Player mirr_player = { .pos = mirrored, .state = PHYSICAL };
+                case_push(p->player_case, mirr_player);
+                INFO("added new at %.0f, %.0f", mirrored.x, mirrored.y);
+            }
+        }
+
+    }
+}
+
+int get_mirror_direction(Puzzle *p, int btn_id)
+{
+    int options = 0;
+    Vector2 m_pos = GetMousePosition();
+    Button sel_ws = p->button_case[p->clicked_button];
+    Button sel_vs = vs_button_of_ws(p, sel_ws);
+    if (sel_ws.center.x == 0.f) {
+        if (m_pos.y >= sel_vs.center.y) {
+            INFO("DOWN");
+            options |= MIRROR_DOWN;
+        } else {
+            INFO("UP");
+            options |= MIRROR_UP;
+        }
+    } else if (sel_ws.center.y == 0.f) {
+        if (m_pos.x >= sel_vs.center.x) {
+            INFO("RIGHT");
+            options |= MIRROR_RIGHT;
+        } else {
+            INFO("LEFT");
+            options |= MIRROR_LEFT;
         }
     } else {
-        ASSERT(0, "Unreachable. invalid button position");
+        ASSERT(0, "Unreachable. New dimension?");
     }
+    return options;
+}
+
+void render_player(Puzzle *p, Player player)
+{
+    float cell_width = p->rec.height / p->cols;
+    Player vs_pos = vs_player_of_ws(p, player);
+    Rectangle dims = {
+        .x = vs_pos.pos.x,
+        .y = vs_pos.pos.y,
+        .width = cell_width,
+        .height = cell_width,
+    };
+    Color color;
+    switch (player.state) {
+        case PHYSICAL: { color = PINK; } break;
+        case PREVIEW: { color = GRAY; } break;
+    }
+    DrawRectangleRec(dims, color);
 }
 
 void render_puzzle(Puzzle *p)
@@ -372,14 +452,36 @@ void render_puzzle(Puzzle *p)
 
     fill_buttons(p);
 
-    // TODO check for mouse movement first
+
     int hover_id = button_hover_id(p, p->button_case);
+    Button vs_btn = vs_button_of_ws(p, p->button_case[hover_id]);
     if (hover_id != -1) {
-        Button vs_btn = vs_button_of_ws(p, p->button_case[hover_id]);
-        DrawCircleV(vs_btn.center, 30.f, MAGENTA);
+        DrawCircleV(vs_btn.center, 50.f, MAGENTA);
+    }
+
+    if (p->clicked_button == -1) {
+        // Step 1: selecting button
+        if (hover_id != -1 && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            // Move to next step
+            p->clicked_button = hover_id;
+        }
+    } else {
+        // Step 2: selecting side
+
+        if (IsKeyPressed(KEY_C)) {
+            // TODO: Is outside of rectangle
+            p->clicked_button = -1;
+        }
+
+        int options = get_mirror_direction(p, p->clicked_button);
+
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            DrawCircleV(vs_btn.center, 50.f, MAGENTA);
-            mirror_over_line(p, hover_id);
+            // Finalize changes. Restart steps
+            mirror_over_line(p, p->clicked_button, options);
+            p->clicked_button = -1;
+        } else {
+            // Preview changes
+            mirror_over_line(p, p->clicked_button, options | MIRROR_PREVIEW);
         }
     }
 
@@ -424,15 +526,45 @@ void render_puzzle(Puzzle *p)
     // Draws player objects
     size_t i;
     for (i = 0; i < case_len(p->player_case); ++i) {
-        Vector2 vs_pos = vs_player_of_ws(p, p->player_case[i]);
-        Rectangle player = {
-            .x = vs_pos.x,
-            .y = vs_pos.y,
-            .width = cell_width,
-            .height = cell_width,
-        };
-        DrawRectangleRec(player, PINK);
-        // INFO("Drawing square %zu at %0.f, %0.f", i, player.x, player.y);
+        render_player(p, p->player_case[i]);
     }
-    render_buttons(p);
+    for (i = 0; i < case_len(p->player_case); ++i) {
+        if (p->player_case[i].state == PREVIEW) {
+            case_len(p->player_case) = i;
+        }
+    }
+
+    if (p->clicked_button != -1) {
+        Button sel_ws = p->button_case[p->clicked_button];
+        Button sel_vs = vs_button_of_ws(p, sel_ws);
+        int options = get_mirror_direction(p, p->clicked_button);
+        Rectangle rec = { 0 };
+        if (options & MIRROR_LEFT) {
+            rec.x = p->rec.x;
+            rec.y = p->rec.y;
+            rec.width = sel_vs.center.x - p->rec.x;
+            rec.height = p->rec.height;
+        } else if (options & MIRROR_RIGHT) {
+            rec.x = sel_vs.center.x;
+            rec.y = p->rec.y;
+            rec.width = p->rec.x + p->rec.width - sel_vs.center.x;
+            rec.height = p->rec.height;
+        } else if (options & MIRROR_UP) {
+            rec.x = p->rec.x;
+            rec.y = p->rec.y;
+            rec.width = p->rec.width;
+            rec.height = sel_vs.center.y - p->rec.y;
+        } else if (options & MIRROR_DOWN) {
+            rec.x = p->rec.x;
+            rec.y = sel_vs.center.y;
+            rec.width = p->rec.width;
+            rec.height = p->rec.y + p->rec.height - sel_vs.center.y ;
+        }
+        DrawRectangleRec(rec, BLUE);
+        render_button(p, sel_ws);
+    } else {
+        for (i = 0; i < case_len(p->button_case); ++i) {
+            render_button(p, p->button_case[i]);
+        }
+    }
 }
