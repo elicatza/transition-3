eba6e88df9bb126f0535d5168e4a3396ecc0de92
elicatza <eliza.cl@protonmail.com>
2024-07-22 10:26:12 +0200

    feat: world infrastructure set up

--- src/main.c
+++ src/main.c
@@ -18,23 +18,109 @@
 #define WIDTH 800
 #define HEIGHT (WIDTH / ASPECT_RATIO)
 
+/**
+ * World format
+ * First two bits are ground type
+ * 0b00: Ground
+ * 0b01: interact
+ * 0b10: light
+ * 0b11: `reserved`
+ *
+ * If interact: Next two bits are interact type
+ * 0b00xx: window blinds
+ * 0b01xx: training puzzle
+ * 0b10xx: bed
+ * 0b11xx: door
+ * 0b11xx: creative puzzles
+ *
+ * If light: Next two bits are light color
+ * 0b00xx: blue
+ * 0b01xx: white
+ * 0b10xx: pink
+ * 0b11xx: black
+ * Then 4 bits for the light strength
+ *
+ * If ground: Next two bits are elevation / type
+ * 0b00xx: empty space
+ * 0b001xx: Elevation 1
+ * 0b010xx: Elevation 2
+ * 0b011xx: Elevation 3
+ * 0b100xx: Player
+ */
 
-// typedef enum {
-//     PUZZLE,
-//     WORLD,
-//     MENU,
-// } GameState;
+enum {
+    TGROUND = 0b00,
+    TLIGHT = 0b01,
+    TINTERACT = 0b10,
+} CellType;
+
+#define MASK_TYPE(a) ((a) & 0b11)
+#define MASK_INTERACT(a) ((a) & 0b1100)
+#define MASK_LIGHT(a) ((a) & 0b1100)
+#define MASK_LIGHT_BRIGHTNESS(a) ((a) & 0b11110000)
+#define MASK_GROUND(a) ((a) & 0b11100)
+
+/**
+ * Header:
+ * cols, rows
+ *
+ * Body: world format (len = width * height)
+ *
+ */
+#define R 0b0100
+static u8 world1[] = {
+    5, 5,
+    R, R, R, R, R,
+    R, R, 0, R, R,
+    R, R, R, R, R,
+    R, R, R, R, R,
+    R, R, R, R, R,
+};
+
+typedef enum {
+    PUZZLE,
+    WORLD,
+    MENU,
+} GameState;
+
+typedef struct U32x2 {
+    u32 x;
+    u32 y;
+} U32x2;
+
+typedef struct Player {
+    U32x2 pos;
+} Player;
+
+typedef struct Cell {
+    U32x2 pos;
+    u8 info;
+} Cell;
+
+typedef struct World {
+    Player player;
+    size_t cols;
+    size_t rows;
+    Cell *cell_case;
+} World;
 
 typedef struct {
     Texture2D atlas;
     Puzzle *puzzle;
+    World *world;
+    GameState state;
 } GO;
 
 GO go = { 0 };
 
+World *load_world(u8 *byte_map);
+void update_world(World *w);
+void render_world(World *w);
+void free_world(World *w);
 
 
 /**
+ * Puzzle format
  * Header: width, height, padding
  * body cell map
  */
@@ -57,7 +143,6 @@ void loop(void);
 
 int main(void)
 {
-
     int width = WIDTH;
     int height = HEIGHT;
     SetConfigFlags(/* FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE | */ FLAG_MSAA_4X_HINT);
@@ -71,10 +156,10 @@ int main(void)
         .mipmaps = 1,
     };
 
-    Puzzle *puzzle = load_puzzle(puzzle1);
-    // case_push(puzzle.player_case, ((Player) { .pos = (Vector2) { 0.f, 0.f }, .state = PHYSICAL, .height = 1 } ));
     go.atlas = LoadTextureFromImage(atlas_img);
-    go.puzzle = puzzle;
+    go.world = load_world(world1);
+    go.puzzle = load_puzzle(puzzle1);
+    go.state = PUZZLE;
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(loop, 0, 1);
@@ -87,6 +172,7 @@ int main(void)
 #endif
 
     UnloadTexture(go.atlas);
+    free_puzzle(go.puzzle);
     CloseWindow();
     return 0;
 }
@@ -96,13 +182,64 @@ void loop(void)
 {
     Puzzle *p = go.puzzle;
 
-    update_puzzle(p);
+    switch (go.state) {
+        case PUZZLE: { update_puzzle(p); } break;
+        default: { ASSERT(0, "Not implemented") } break;
+    }
 
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-    render_puzzle(p, go.atlas);
+    switch (go.state) {
+        case PUZZLE: { render_puzzle(p, go.atlas); } break;
+        default: { ASSERT(0, "Not implemented"); } break;
+    }
 
     EndDrawing();
+}
+
+void update_world(World *w)
+{
+}
+
+void render_world(World *w)
+{
+}
+
+void fill_wcells(World *w, u8 *wbody)
+{
+    size_t row, col;
+    for (row = 0; row < w->rows; ++row) {
+        for (col = 0; col < w->cols; ++col) {
+            u8 type = MASK_TYPE(wbody[row * w->cols + col]);
+            switch (type) {
+                case TGROUND: { INFO("We hit ground"); } break;
+                default: { ASSERT(0, "Invalid world cell type"); } break;
+            }
+            Cell cell;
+            cell.info = wbody[row * w->cols + col];
+            cell.pos = (U32x2) { col, row };
+            case_push(w->cell_case, cell);
+        }
+    }
+}
+
+World *load_world(u8 *wmap)
+{
+    World *w = malloc(sizeof *w);
+    size_t cols = *wmap; ++wmap;
+    size_t rows = *wmap; ++wmap;
+
+    w->cols = cols;
+    w->rows = rows;
+    w->cell_case = case_init(cols * rows, sizeof *w->cell_case);
+
+    fill_wcells(w, wmap);
+    return w;
+}
+
+void free_world(World *w)
+{
+    free(w);
 }
 
